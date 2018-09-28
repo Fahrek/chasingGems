fbafd51406364944369d46654a3479af97ae00aa
Fahrek <andresgqjob@gmail.com>
2018-09-28 19:22:36 +0200

    Fitting Screen & Loading Images

--- src/gameplay.js
+++ src/gameplay.js
@@ -1,15 +1,30 @@
 GamePlayManager = {
     init: function()
     {
-        console.log('init');
+        //Centramos y ajustamos la pantalla
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        game.scale.pageAlignVertically   = true;
+        game.scale.pageAlignHorizontally = true;
     },
     preload: function()
     {
-        console.log('preload');
+        // Cargamos los objetos
+        game.load.image("background", "assets/img/background.png");
+        game.load.spritesheet('seahorse', 'assets/img/horse.png', 84, 156, 2);
     },
     create: function()
     {
-        console.log('create');
+        // Mostramos los objetos en pantalla
+        game.add.sprite(0, 0, 'background');
+        // Guardamos la instancia del caballo en una variable para poder trabajar
+        this.seahorse = game.add.sprite(0, 0, 'seahorse');
+        // Aquí le estamos diciendo que de la hoja de sprites utilice el segundo frame según lista en base cero
+        this.seahorse.frame = 1;
+        // Aquí posicionamos el elemento en el centro de la pantalla respecto el eje X (Horizontal) e Y (Vertical)
+        this.seahorse.x = game.width/2;
+        this.seahorse.y = game.height/2;
+        // Poscionamos el 'anchor' del elemento en el centro del mismo
+
     },
     update: function()
     {
@@ -17,7 +32,7 @@ GamePlayManager = {
     },
 };
 
-var game = new Phaser.Game(1136, 640, Phaser.AUTO);
+var game = new Phaser.Game(1136, 640, Phaser.CANVAS);
 
 game.state.add('gameplay', GamePlayManager);
 game.state.start('gameplay');
